2d9b32808b88be35499a3ffedca44ec49c5d5b00
AlexanderSinclairTeixeira <alesintex@gmail.com>
2022-02-28 17:14:11 +0000

    decoding 1st attempt

--- keypad.s
+++ keypad.s
@@ -2,12 +2,14 @@
 
 ;extrn delay_W_ms, delay_W_4us, delay_count_250ns, delay_500ns
 extrn delay_W_4us
-global keypad_setup, keypad_read
+global keypad_setup, keypad_read, keypad_decode
 
 psect udata_acs
 
-col_row_ix EQU 0x10
-decode_val EQU 0x20
+col_ix EQU 0x10
+row_ix EQU 0x11
+temp_counter EQU 0x12
+decode_val EQU 0x13
     
 psect code
  
@@ -25,40 +27,68 @@ keypad_read:
     movwf TRISE, A
     movlw 1
     call delay_W_4us
-    movff PORTE, col_row_ix
-    
+    movff PORTE, col_ix
+    comf col_ix, F, A
+
     ; PORTE<4:7> inputs, PORTE<0:3> outputs
     movlw 0b11110000
     movwf TRISE, A
     movlw 1
     call delay_W_4us
-    movf PORTE, W, A
-    
-    iorwf col_row_ix, F, A
-    comf col_row_ix, F, A
-    
-    movff col_row_ix, PORTD, A
+    movff PORTE, row_ix
+    comf row_ix, F, A
     return
 
 keypad_decode:
-        
-    retlw '1'
-    retlw '2'
-    retlw '3'
-    retlw 'A'
-    
-    retlw '4'
-    retlw '5'
-    retlw '6'
-    retlw 'B'
-    
-    retlw '7'
-    retlw '8'
-    retlw '9'
-    retlw 'C'
-    
-    retlw '*'
-    retlw '0'
-    retlw '#'
-    retlw 'D'
+    movf row_ix, W, A
+    andwf col_ix, W, A
+    tstfsz WREG, A
+    goto nonzero
+    retlw 0
+    nonzero:
+	swapf row_ix, F, A
+	movlw 0 
+	movwf decode_val, A
+	movlw 4
+	movwf temp_counter, A
+    loop1:
+	btfss row_ix, 0, A
+	goto loop2
+	rrcf row_ix, F, A
+	bcf STATUS, STATUS_C_POSN, A
+	addwf decode_val, F, A
+	decfsz temp_counter, F, A
+	goto loop1
+    movwf temp_counter, A
+    loop2:
+	btfss col_ix, 0, A
+	goto lookup
+	rrcf col_ix, F, A
+	bcf STATUS, STATUS_C_POSN, A
+	incf decode_val, F, A
+	decfsz temp_counter, F, A
+	goto loop2
+    lookup:
+    goto $ + 2 + decode_val
+	retlw '1'
+	retlw '2'
+	retlw '3'
+	retlw 'A'
+
+	retlw '4'
+	retlw '5'
+	retlw '6'
+	retlw 'B'
+
+	retlw '7'
+	retlw '8'
+	retlw '9'
+	retlw 'C'
+
+	retlw '*'
+	retlw '0'
+	retlw '#'
+	retlw 'D'
+
+	retlw 'X'
     
--- main.s
+++ main.s
@@ -1,7 +1,7 @@
 #include <xc.inc>
 
 ;extrn	UART_Setup, UART_Transmit_Message   ; UART stuff
-extrn	LCD_Setup, LCD_Write_Message	    ; LCD stuff
+;extrn	LCD_Setup, LCD_Write_Message	    ; LCD stuff
 ;extrn	delay_W_ms, delay_W_4us, delay_count_250ns, delay_500ns ;from utils
 ;extrn	mul_u16_x_u16 ;from utils
 ;extrn	counter_low, counter_high, ms_counter	    ;from utils
@@ -16,11 +16,11 @@ res3:	ds 1
 res4:	ds 1
 
     
-psect code
+psect code, abs
 
 setup:
     ;call UART_Setup
-    call LCD_Setup
+    ;call LCD_Setup
     call keypad_setup
     goto start
 start:
