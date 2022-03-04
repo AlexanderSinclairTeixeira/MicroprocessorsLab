6a3ad9f74b1918be7bbf15cb49e5f123e9157384
DELL <DELL@DESKTOP-4RGQ0M8>
2022-03-04 12:47:58 +0000

    this one works! bossman delay everywhere

--- glcd_utils.s
+++ glcd_utils.s
@@ -7,174 +7,184 @@
 #define    GLCD_E   4 ;clock: cycle time 1us and triggers on falling edge
 #define    GLCD_RST 5 ;reset (active high so write 1 for reset)
 
-global glcd_setup, psel_W, ysel_W, write_strip_W
-global glcd_status, glcd_read, glcd_page, glcd_y, glcd_strip
-    
+global glcd_setup,psel_W,ysel_W,write_strip_W,LCD_delay_ms,bossman_delay
+
 psect udata_acs
     glcd_status EQU 0x00
     glcd_read EQU 0x01
     glcd_page EQU 0x02
     glcd_y EQU 0x03
-    glcd_strip EQU 0x04
+    glcd_write EQU 0x04
+    glcd_strip EQU 0x05
+ 
+    LCD_cnt_l:	ds 1   ; reserve 1 byte for variable LCD_cnt_l
+    LCD_cnt_h:	ds 1   ; reserve 1 byte for variable LCD_cnt_h
+    LCD_cnt_ms:	ds 1   ; reserve 1 byte for ms counter
+    LCD_tmp:	ds 1   ; reserve 1 byte for temporary use
+    LCD_counter:	ds 1   ; reserve 1 byte for counting through nessage    
  
 psect glcd_code, class=CODE
-
-; main functions to control the display
-; 
-; glcd_setup, glcd_on, glcd_off, psel_W, ysel_W, write_strip_W, read_data_read_status
-; above all have timing included
-; display_start not implemented yet
-
+    
 glcd_setup:
+    call bossman_delay
     movlw 0x00
+    call bossman_delay
     movwf TRISB, A ;port B is output
+    call bossman_delay
     movwf TRISD, A ;port D is output
-    clrf LATD, A
-    call delay_1us ;stabilise
+    call bossman_delay
+    call chip_select
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
     call glcd_on
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    call chip_select
     return
 
-glcd_on:
-    call write_inst_pin_setup
-    movlw 0b00111111 ;last bit set for on
-    call send_D_and_clock
+chip_select:
+    call bossman_delay
+    bcf PORTB, GLCD_CS1, A
+    call bossman_delay
+    bsf PORTB, GLCD_CS2, A
+    call bossman_delay
     return
     
-glcd_off:
-    call write_inst_pin_setup
-    movlw 0b00111110 ;last bit clear for off
-    call send_D_and_clock
+glcd_on:
+    call bossman_delay
+    bcf PORTB, GLCD_RS, A ;instruction
+    call bossman_delay
+    bcf PORTB, GLCD_RW, A ;writing
+    call bossman_delay
+    movlw 0b00111111 ;last bit set for on
+    call bossman_delay
+    movwf PORTD, A
+    call bossman_delay
+    call clock
     return
-   
+
 psel_W:
-    movwf glcd_page
-    call wait_till_free
-    call write_inst_pin_setup
     ;WREG contains a number from 0b000 - 0b111 i.e. the page number 0 - 7
     ;now set the page on the chip from the value in working register
-    movf glcd_page, A
-    ;movlw 2 ;;;;;;;;;;; DEBUG
-    addlw 0b10111000 ;turn into page select instruction
-    call send_D_and_clock
-    return
+    call	bossman_delay
+    call LCD_delay_ms
+    call	bossman_delay
+    bcf PORTB, GLCD_RS, A ;instruction
+    call	bossman_delay
+    bcf PORTB, GLCD_RW, A ;writing
+    call	bossman_delay
+    ;movlw 0 ;debug----------------------------
+    call	bossman_delay
+    addlw 0b10111000 ;turn into page select instructio
+    call	bossman_delay
+    movwf PORTD, A
+    call bossman_delay
+    call clock
+    return    
     
 ysel_W:
-    movwf glcd_y
-    call wait_till_free
-    call write_inst_pin_setup
     ;select the y adress from WREG 0b00000000 - 0b01111111, i.e. 0 - 127
     ;now set the strip on the page from the value in working register
-    call csel_L ;assume left, i.e. 0 <= W < 64
-    movf glcd_y
-    btfsc WREG, 6, A ;skip the next instruction if bit 6 of W is clear
-    call csel_R ;if it is set, we are in 64-127, so on the right chip
-    ;movlw 0 ;;;;;;;;;;;;;DEBUG
-    bsf WREG, 6, A ;turn into instruction
-    call send_D_and_clock
-    return
-
-;display_start:
-    ;used for scrolling i think?
-    ;call write_inst
-    ;needs implementing (maybe?)
-    ;return
+    call bossman_delay
+    bcf PORTB, GLCD_RS, A ;instruction
+    call bossman_delay
+    bcf PORTB, GLCD_RW, A ;writing
+    call bossman_delay
+    movlw 0 ;debug----------------------------
+    call bossman_delay
+    movwf PORTD, A
+    call bossman_delay
+    bsf PORTD, 6, A ;turn into instruction
+    call bossman_delay
+    call clock
+    return    
     
 write_strip_W:
     ;write a pixel strip from W to glcd ram
     ;increases y address automatically
-    movwf glcd_strip
-    call wait_till_free
-    call write_data_pin_setup
-    movf glcd_strip, A
-    ;movlw 0b11010010 ;;;;;;;DEBUG
-    call send_D_and_clock
-    return
-
-read_data:
-    call wait_till_free
-    call read_data_pin_setup
-    movlw 0xFF
-    movwf TRISD, A ;set PORTD as input
-    clrf LATD, A ;dont want any interference
-    call clock ;send instruction and get the data
-    movff PORTD, glcd_read, A
-    movlw 0x00
-    movwf TRISD, A ;PORTD back to output
-    clrf LATD, A
-    call delay_1us
-    return
-   
-read_status:
-    ;B0PR0000
-    ;B=Busy: 0-ready, 1-in operation
-    ;P=power: 0-on, 1-off
-    ;R=Reset: 0-normal, 1-reset
-    movlw 0xFF
-    call read_inst_pin_setup
-    movwf TRISD, A ;set PORTD as input
-    clrf LATD, A ;dont want any interference
-    call clock ;send instruction and get the data
-    movff PORTD, glcd_status, A
-    movlw 0x00
-    movwf TRISD, A;PORTD back to output
-    clrf LATD, A
-    call delay_1us
-    return
-    
-wait_till_free:
-    call read_status
-    btfsc glcd_status, 7, A ;top bit is "Busy"
-    goto wait_till_free
-    return
-   
-;inner function calls to save being repetitive
-send_D_and_clock:
+    call bossman_delay
+    bsf PORTB, GLCD_RS, A ;data
+    call bossman_delay
+    bcf PORTB, GLCD_RW, A ;writing
+    call bossman_delay
+    ;movlw 0b00000000 ;debug----------------------------
+    call bossman_delay
     movwf PORTD, A
     call clock
-    ;clrf LATD, A
-    call delay_1us
     return
-
-read_inst_pin_setup:
-    bcf PORTB, GLCD_RS, A ;instruction
-    bsf PORTB, GLCD_RW, A ;reading
-    return
-
-read_data_pin_setup:
-    bsf PORTB, GLCD_RS, A ;data
-    bsf PORTB, GLCD_RW, A ;reading
-    return
-
-write_inst_pin_setup:
-    bcf PORTB, GLCD_RS, A ;instruction
-    bcf PORTB, GLCD_RW, A ;writing
-    return
-
-write_data_pin_setup:
-    bsf PORTB, GLCD_RS, A ;data
-    bcf PORTB, GLCD_RW, A ;writing
-    return
-
-csel_L:
-    bcf PORTB, GLCD_CS1, A    ;set the cs0 pin
-    bsf PORTB, GLCD_CS2, A    ;clear the cs1 pin
-    return
-
-csel_R:
-    bsf PORTB, GLCD_CS1, A    ;clear the cs0 pin
-    bcf PORTB, GLCD_CS2, A    ;set the cs1 pin
-    return
-
-;timing stuff
+    
 clock: ;set the clock to run (falling edge)
+    call bossman_delay
     bsf PORTB, GLCD_E, A
-    call delay_1us
+    call bossman_delay
     bcf PORTB, GLCD_E, A
-    call delay_1us
+    call bossman_delay
     return
-   
-delay_1us: ;16 instructions * 4 Q cycles @ 64MHz = 1us delay
-    REPT 32
-	nop
+    
+    
+    
+; ** a few delay routines below here as LCD timing can be quite critical ****
+bossman_delay:
+    REPT 0xFF
+	call LCD_delay_ms
     ENDM
-    return
+LCD_delay_ms:		    ; delay given in ms in W
+	nop
+	movwf	LCD_cnt_ms, A
+	return
+lcdlp2:	movlw	250	    ; 1 ms delay
+	call	LCD_delay_x4us	
+	decfsz	LCD_cnt_ms, A
+	bra	lcdlp2
+	return
+    
+LCD_delay_x4us:		    ; delay given in chunks of 4 microsecond in W
+	movwf	LCD_cnt_l, A	; now need to multiply by 16
+	swapf   LCD_cnt_l, F, A	; swap nibbles
+	movlw	0x0f	    
+	andwf	LCD_cnt_l, W, A ; move low nibble to W
+	movwf	LCD_cnt_h, A	; then to LCD_cnt_h
+	movlw	0xf0	    
+	andwf	LCD_cnt_l, F, A ; keep high nibble in LCD_cnt_l
+	call	LCD_delay
+	return
+
+LCD_delay:			; delay routine	4 instruction loop == 250ns	    
+	movlw 	0x00		; W=0
+lcdlp1:	decf 	LCD_cnt_l, F, A	; no carry when 0x00 -> 0xff
+	subwfb 	LCD_cnt_h, F, A	; no carry when 0x00 -> 0xff
+	bc 	lcdlp1		; carry, then loop again
+	return			; carry reset so return
+
+
+    end
+
--- main.s
+++ main.s
@@ -1,11 +1,11 @@
 #include <xc.inc>
 
-extrn glcd_setup, psel_W, ysel_W, write_strip_W
-extrn glcd_status, glcd_read, glcd_page, glcd_y, glcd_strip
+extrn glcd_setup,psel_W,ysel_W,write_strip_W,LCD_delay_ms, bossman_delay
 
 
 psect udata_acs
     y_counter EQU 0x10
+    page_counter EQU 0x11
  
 psect code, abs
 rst:
@@ -16,24 +16,68 @@ int_hi:
     org 0x08	;high interrupt vector
     
 setup:
+    movlw 0
+    movwf y_counter
+    movwf page_counter
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
     call glcd_setup
-    movlw 0x0
-    call ysel_W
-    movlw 0x0
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    movlw 2 
     call psel_W
-    movlw 0x4
-    movwf y_counter, A
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    
+    call ysel_W
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    call bossman_delay
+    goto start
+    
+write_byte:
+    call bossman_delay
+    call bossman_delay
+    movlw 0b00000000
+    call write_strip_W
+    call bossman_delay
+    call bossman_delay
+    return
+start:
+    call write_byte
+    
     goto start
 
-start:
-    movlw 0b00110011
-    call write_strip_W
-    decfsz y_counter,A
-    goto start
-    movlw 4
-    movlw 127
-    movwf y_counter, A
-    goto start
     goto $
+    
 
 end	    rst
